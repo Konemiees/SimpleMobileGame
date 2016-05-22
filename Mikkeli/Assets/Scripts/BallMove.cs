fa3ac55fdd38852f2cdced75f6685efa51a5cb2b
Konemiees <konsta.sinisalo@gmail.com>
2016-05-22 17:56:14 +0300

    More detail, empty scripts

---
+++ Mikkeli/Assets/Scripts/BallMove.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class BallMove : MonoBehaviour {
+
+	public float Speed = 3.0f;
+	private float rotation;
+
+
+	// Use this for initialization
+	void Start () {
+		rotation = 0.001f * Random.Range (-3, 3);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
