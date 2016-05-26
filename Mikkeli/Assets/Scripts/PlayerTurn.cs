f904f7c4c56be407e4c01c89f168c2a171d7545a
Konemiees <konsta.sinisalo@gmail.com>
2016-05-26 17:53:58 +0300

    Working player turning

--- Mikkeli/Assets/Scripts/PlayerTurn.cs
+++ Mikkeli/Assets/Scripts/PlayerTurn.cs
@@ -3,6 +3,10 @@ using System.Collections;
 
 public class PlayerTurn : MonoBehaviour {
 
+	public float turnSpeed = 2;
+
+
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +14,9 @@ public class PlayerTurn : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		float rotateTo = transform.rotation.z; 
+		if (Input.touchCount > 0 && Input.GetTouch (0).phase == TouchPhase.Moved) {
+			rotateTo = -Input.GetTouch(0).deltaPosition.x * turnSpeed * Time.deltaTime + rotateTo;
+		} transform.rotation = Quaternion.Euler(0, 0, rotateTo);
 	}
 }
---
+++ Mikkeli/Assets/Scripts/TestPlayerScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestPlayerScript : MonoBehaviour {
+
+	public float turnSpeed = 2;
+	private float rotateTo;
+
+	// Use this for initialization
+	void Start () {
+		rotateTo = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float change = -Input.GetAxisRaw ("Mouse X") * turnSpeed;
+		if (Input.GetMouseButton(0) && change != 0) {
+			rotateTo = change + rotateTo;
+		}while (rotateTo > 90)
+			rotateTo = rotateTo - 90;
+		while (rotateTo < 0)
+			rotateTo = + 90;
+		transform.rotation = Quaternion.Euler(0, 0, rotateTo);
+	}
+}
